a7ecb635fe6eed57d20ab6201e4f7f221247bdb9
Pavan <pavanskollimarla@gmail.com>
2023-10-01 21:35:05 -0500

    refactor-inti-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { RouterModule, Routes, ExtraOptions,InitialNavigation } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileModule } from './profile/profile.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,8 +23,8 @@ const routes: Routes = [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-}),
+      initialNavigation: 'enabled' as InitialNavigation
+    }),
     ProfileModule,
     HttpClientModule
   ],
--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -183,11 +183,11 @@ export class ProfileService {
   ]
   about2 = `Software Developer with 2.5+ years of experience .
   Worked with Software Development Life Cycle(SDLC) and  agile methodologies .
-  Expertise in Angular 2+ All versions, React, Javascript,  Node JS and UI with Responsive Designs .
-  Have good understanding of Python, Jquery, Bootstrap, MongoDB, Firebase, and MYSQL`
+  Expertise in Angular 2+ & Above, Javascript, Java, SpringBoot and UI with Responsive Designs .
+  Have good understanding of Python, MYSQL, CI/CD, Maven and other technologies required for robust web development`
 
   about = "Lover of innovation and everything related to generate new knowledge.Face problems with a smile and solve them as soon as possible. Very calculated about the time I spend and work I do."
-  resumeurl = "https://docs.google.com/document/d/1WODFuhIzQw2AXPsjt6f3rs-EDWhdhT8s89fOTrGRh70/edit?usp=sharing"
+  resumeurl = "https://drive.google.com/file/d/1V0ybzqW33xyQiviJ09YxVt1OK7kWKYWo/view?usp=drivesdk"
 
   skillsData: any = [
     {
@@ -224,84 +224,32 @@ export class ProfileService {
 
 
   educationData: any = [
-    {
-      'id': '1',
-      'from_to_year': '2015 - 2019',
-      'education': 'Bachelor\'s Degree',
-      'stream': 'Bachelor of Engineering',
-      'info': `Aditya Silver Oak Institute is GTU Affiliated and ranks 7th in Gujurat
-Completed B.E in Computer Engineering with 9.54 CGPA.
-Won the Best Student Award 3 times for excellent Academic records at College.
-Maintained above 9.1 spi in every semester of college academic .`,
-      'institution': 'ADITYA SILVER OAK INSTITUTE OF TECHNOLOGY, AHMEDABAD'
-    },
-    {
-      'id': '2',
-      'from_to_year': '2013 - 2015',
-      'education': 'Higher Secondary',
-      'stream': 'Science and Mathematics',
-      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
-      'info': `The High School was mainly focus on the bases of science, mathematics, and Chemistry
-There was also a good base on physics, mathematics and chemistry.
-Completed my high school with 78%.`
-    },
-    {
-      'id': '3',
-      'from_to_year': '2012 - 2013',
-      'education': 'Secondary  School',
-      'stream': 'Science and Mathematics',
-      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
-      'info': `The Secondary aims at Maths , English , Science, Social Science, Litrature and Languages.
-Completed my Secondary school with 84%.`
-    }
+    
   ];
+
   exprienceData: any = [
     {
-      id: 4,
-      company: 'Crest Data System',
-      location: 'Ahmedabad',
-      timeline: 'Aug 2021 to Present',
-      role: 'Software Developer',
-      work: 'Working as a full stack developer .Responsible for handling the UI in React and Managing the api calls in python Flask.'
+      id: 3,
+      company: 'Copart Inc',
+      location: 'Dallas, Texas',
+      timeline: 'May 2023 to Present',
+      role: 'Software Engineer Intern',
 
     },
     {
-      id: 3,
-      company: 'Indianic Info Tech',
-      location: 'Ahmedabad',
-      timeline: 'Feb 2021 to June 2021',
-      role: 'Software Developer',
-      work: `Technologies : Angular , Node JS , MongoDB .
-      Worked on the web application built in Angular for different business and Campaigns .
-      Contributed on developing the admin dashboard with different charts and insights for the application.`
+      id: 2,
+      company: 'Texas Tech University',
+      location: 'Lubbock, Texas',
+      timeline: 'March 2022 to May 2023',
+      role: 'Graduate Assitant',
+     
     },
-  //   {
-  //     id: 2,
-  //     company: 'Growth99',
-  //     location: 'Remote',
-  //     timeline: '',
-  //     role: 'Frontend  Developer',
-  //     work: `Technologies : Angular , Node JS , MongoDB .
-  // Worked on the product of Growth99 which allowed other clients to come and integrate our product in their website .
-  // Worked on REST Api , Admin Dashboard , Appointment Booking for services and UI of the application .
-  // Created sections like templates for email, sms, landing pages, clinic, services, providers and triggers Automation.
-  // `
-  //   },
-
     {
       id: 1,
-      company: 'Tata Consultancy Services',
-      location: 'Gandhinagar and Pune',
-      timeline: 'June 2019 - Jan 2021',
-      role: 'Assistant System Engineer Trainee',
-      work: `Technologies : Angular , JavaSpring Boot , SQL. Apache Solr .
-
-  Project 1 : Worked on uploading two excel sheets with bulk of data and finding the matches for each business .
-
-  Project 2 : Worked on indexing the search results inside the application with bulk of data using Apache Solr and configuring the server with cron jobs and shell scripts .
-
-  Worked on ranking the search results based on various criteria,features and delivering the accurate results based on generated scores.
-  `
+      company: 'Copart India Technology Center',
+      location: 'Hyderabad, Telangna',
+      timeline: 'April 2021 - Dec 2021',
+      role: 'Software Engineer',
     },
 
 
